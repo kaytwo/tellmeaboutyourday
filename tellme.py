dd68c03fcaf11126101cb2e21d3362d8cac2d506
Chris Kanich <ckanich@uic.edu>
2014-04-30 15:50:42 -0500

    checkpoint, connection basics, class based structure

--- tellme.py
+++ tellme.py
@@ -4,6 +4,7 @@ from os import system
 import sys
 import re
 import twitter
+from pprint import pprint
 
 import smtplib
 from email.mime.text import MIMEText
@@ -14,15 +15,25 @@ tellmeaboutyourday
 
 using @livetellshowsee for development purposes
 
+main tasks:
 respond to DMs, schedule periodic DMs based on message content
 organize the DM responses into a database that will be rendered by a webapp
 
 first iteration: respond to messages based on user state
-"good morning" -> "good morning! I will ask you to tell me about every hour of today until you message me the word 'goodnight'" -> schedule all messages including immediately right now
+"good morning" 
+  -> "good morning! I will ask you to tell me about 
+    every hour of today until you message me the word 'goodnight'" 
+  -> schedule all messages including immediately right now
 if receive message from someone active, say "okay, thank you, I will get back to you in an hour."
 "goodnight" -> "thank you for sharing your day. I will not message you again unless you message me 'good morning' again."
 
 on scheduled message: "hi again! Tell me about something you thought, saw, did, or felt this hour. You can attach a picture if you like."
+
+problems:
+  twitter api doesn't have an "accept follower" call, 'accept' and 'deny' rest endpoints might work, TODO.
+    this will only matter if acct is protected. current workaround: different account for bot and output
+    other option is to manually update accepted users, PITA but nbd, premature optimization
+  to remove a follower, block then unblock them.
 
 
 improvements:
@@ -32,19 +43,18 @@ improvements:
       * if more than 5 messages to send, ABORT w/email
   * log to backing store for all messages sent/received w/ pictures
   * BACKING STORE! WEBAPP!
+  * pull list of subscribers from db, update following/allowed followers
 
-if anything ever goes horribly wrong, email me instead
+if anything ever goes horribly wrong, email me
 
 
 '''
-
-
 
 def error_out(msg,alert=False):
   if alert:
     # increment the comicnum so we don't get repeated alerts
     emsg = MIMEText(msg)
-    emsg['Subject'] = 'trigramosaurus error!'
+    emsg['Subject'] = 'Tellme error!'
     emsg['From'] = 'kaytwo@kaytwo.org'
     emsg['To'] = 'kaytwo@gmail.com'
     # this smtp server might change but oh well...
@@ -54,74 +64,57 @@ def error_out(msg,alert=False):
     print msg
   sys.exit()
 
-def post_to_twitter(msg):
-  '''
-  todo:
-  check if trigramosaurus has been updated today, if so, skip updating.
-  '''
-  oauth_token, oauth_secret = twitter.read_token_file('accountucredentials')
-  consumer_key, consumer_secret = twitter.read_token_file('app_credentials')
-  t = twitter.Twitter(
-            auth=twitter.OAuth(oauth_token, oauth_secret,
-                       consumer_key, consumer_secret)
-           )
-  try:
-    result = t.statuses.update(status=msg)
-  # invalid status causes twitter.api.TwitterHTTPError
-  except:
-    error_out("some sort of twitter error")
-  return result
+class Tellme(object):
 
-def update_comicnum(today):
-  ''' read and return the comic number for today.
-  regardless of errors, try to put today's comic number
-  in the file.'''
-  curnum = -1
-  try:
-    cn = open('trigramosaurus.txt','r+')
-    try:
-      curnum = int(cn.read().strip())
-    except:
+  rest_t = None
+  stream_t = None
+  conversation_states = {}
+  my_id = None
+
+  def __init__(self):
+    self.connect_to_twitter()
+    self.my_id = self.rest_t.account.verify_credentials()['id']
+
+
+  def connect_to_twitter(self):
+    oauth_token, oauth_secret = twitter.read_token_file('account_credentials')
+    consumer_key, consumer_secret = twitter.read_token_file('app_credentials')
+    authpoop = twitter.OAuth(oauth_token, oauth_secret,
+                         consumer_key, consumer_secret)
+
+    self.rest_t = twitter.Twitter(auth=authpoop )
+    self.stream_t = twitter.TwitterStream(domain='userstream.twitter.com',auth=authpoop,timeout=30)
+    print "connected to twitter."
+    print dir(self.rest_t)
+
+
+  def choose_response(self,sid,message_text):
+    if sid not in self.conversation_states:
       pass
-    cn.seek(0)
-    cn.truncate()
-    cn.write(str(today))
-    cn.close()
-    if curnum != -1:
-      return curnum
-    else:
-      error_out("could not determine current comic number, reset it to %d" % today,True)
-  except Exception as e:
-    error_out("couldn't read comic number: "+e.message,True)
+    pass
+
+  def respond_to_message(self,msg):
+    sid = msg['sender_id']
+    message_text = msg['text']
+    response = self.choose_response(sid,message_text)
+    if response:
+      self.rest_t.direct_messages.new(user_id=sid,text=response)
+
+  def follow_back(self,msg):
+    ''' check if following, if not, follow'''
+    pass
+
+
+
+  def dm_loop(self):
+    print self.stream_t
+    for msg in self.stream_t.user():
+      if 'direct_message' in msg:
+        self.respond_to_msg(msg['direct_message'])
+      elif 'event' in msg and msg['event'] == 'follow':
+        self.follow_back(msg)
+      pprint(msg)
 
 if __name__ == "__main__":
-  comicid = ''
-  if len(sys.argv) == 2:
-    comicid = '?comic=' + str(sys.argv[1])
-  today_comic = urlopen('http://www.qwantz.com/index.php' + comicid)
-  result = today_comic.read()
-  todayparsed = fromstring(result)
-  try:
-    comicurl = todayparsed.cssselect('img.comic')[0].attrib['src']
-    comicnum = int(re.findall(r'comic2-([0-9]+)\.png',comicurl)[0])
-  except Exception as e:
-    error_out("failed parsing qwantz.com: " + e.message,True)
-  if comicid == '':
-    prev_comic = update_comicnum(comicnum)
-    if comicnum == prev_comic:
-      error_out("same comic")
-    if comicnum != prev_comic + 1:
-      error_out("unexpected comic number: previous was %d, this was %d" % (prev_comic,comicnum),True)
-  system('curl %s > /tmp/comic.png' % comicurl)
-  d = Dinocr('/tmp/comic.png')
-  if d.erased_pixels > 2000:
-    error_out('large amount of erases in a new comic (%d erases, %d uncertainty)' % (d.erased_pixels,d.uncertainty),True)
-  trigram = d.choose_random_trigram()
-  anything = False
-  # don't acept a trigram that has a non-alphanumeric word in it
-  if any([all([not x.isalnum() for x in word]) for word in trigram.split()]):
-    error_out('words in this trigram are weird: %s' % trigram,True)
-  result = post_to_twitter(trigram)
-  infofile = open('trigramosaurus.txt','w')
-  infofile.write(str(comicnum))
-  infofile.close()
+  tellme = Tellme()
+  tellme.dm_loop()
