25a698943eb39a3173ee8bf13fef9867a09a1f28
Chris Kanich <ckanich@uic.edu>
2014-05-01 14:14:09 -0500

    state machine kind of works now, nothing is being saved

--- tellme.py
+++ tellme.py
@@ -3,6 +3,7 @@ from urllib2 import urlopen
 from os import system
 import sys
 import re
+import time
 import twitter
 from pprint import pprint
 
@@ -64,7 +65,7 @@ def error_out(msg,alert=False):
     print msg
   sys.exit()
 
-class Tellme(object):
+class TellMe(object):
 
   rest_t = None
   stream_t = None
@@ -84,14 +85,27 @@ class Tellme(object):
 
     self.rest_t = twitter.Twitter(auth=authpoop )
     self.stream_t = twitter.TwitterStream(domain='userstream.twitter.com',auth=authpoop,timeout=30)
-    print "connected to twitter."
-    print dir(self.rest_t)
 
+  def check_deadlines(self):
+    now = time.time()
+    for user in self.conversation_states.keys():
+      if self.conversation_states[user]['state'] == 'participating':
+        if self.conversation_states[user]['deadline'] <= now:
+          self.rest_t.direct_messages.new("what did you do, think, or see this hour?")
+          self.conversation_states[user]['deadline'] = time.time() + 60*60
 
   def choose_response(self,sid,message_text):
-    if sid not in self.conversation_states:
-      pass
-    pass
+    if sid not in self.conversation_states and message_text == 'good morning':
+      self.conversation_states[sid] = {'state':'participating','deadline':(time.time()+60*60),'firstmsg':time.time()}
+      return "thank you for participating. I will message you once an hour to ask you about your day. What's happening now?"
+    elif sid in self.conversation_states:
+      if self.conversation_states[sid]['state'] == 'participating':
+        if message_text == 'goodnight':
+          self.conversation_states[sid]['state'] = 'asleep'
+          return "thank you for sharing your day"
+        print "RECEIVED MESSAGE TO SAVE, SAVE TIMESTAMP, USER, TEXT, PICTURE"
+        self.conversation_states[sid]['deadline'] = time.time() + 60*60
+        return "thank you. I will message you again in an hour."
 
   def respond_to_message(self,msg):
     sid = msg['sender_id']
@@ -100,21 +114,29 @@ class Tellme(object):
     if response:
       self.rest_t.direct_messages.new(user_id=sid,text=response)
 
-  def follow_back(self,msg):
-    ''' check if following, if not, follow'''
-    pass
+  def follow_back(self,follower_id):
+    ''' check if following, if not, follow
+    this will bomb if the acct has more than 5000 followers,
+    but that's a good problem to have.'''
+    if follower_id not in self.rest_t.friends.ids()['ids']:
+      print self.rest_t.friendships.create(user_id=follower_id)
+      print "sent follow command for %d" % follower_id
+    else:
+      print "already following %d" % follower_id
 
 
 
   def dm_loop(self):
-    print self.stream_t
     for msg in self.stream_t.user():
-      if 'direct_message' in msg:
-        self.respond_to_msg(msg['direct_message'])
-      elif 'event' in msg and msg['event'] == 'follow':
-        self.follow_back(msg)
-      pprint(msg)
+      if 'direct_message' in msg and msg['direct_message']['recipient']['id'] == self.my_id:
+        self.respond_to_message(msg['direct_message'])
+      elif 'event' in msg:
+        if msg['event'] == 'follow' and msg['target']['id'] == self.my_id:
+          self.follow_back(msg['source']['id'])
+      if 'timeout' in msg and msg['timeout'] == True:
+        self.check_deadlines()
+      pprint(self.conversation_states)
 
 if __name__ == "__main__":
-  tellme = Tellme()
+  tellme = TellMe()
   tellme.dm_loop()
